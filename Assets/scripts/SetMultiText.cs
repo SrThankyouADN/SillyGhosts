1322067e853dc17267842eb2a068521bbc391af5
SrThankyouADN <sr.thankyouadn@gmail.com>
2015-04-01 10:47:24 -0300

    By Source tree

--- Assets/scripts/SetMultiText.cs
+++ Assets/scripts/SetMultiText.cs
@@ -5,27 +5,28 @@ using UnityEngine.UI;
 public class SetMultiText : MonoBehaviour
 {
 
-		public Text txt;
-		public static int MultiTxt;
-		public Text PopText;
+	public Text txt;
+	public static int MultiTxt;
+	public Text PopText;
+	//testre
 	
 		
-		// Use this for initialization
-		void Start ()
-		{
-				//txt = gameObject.GetComponent<Text> (); 
-				txt.text = "" + MultiTxt; 
+	// Use this for initialization
+	void Start ()
+	{
+		//txt = gameObject.GetComponent<Text> (); 
+		txt.text = "" + MultiTxt; 
 		
-		}
+	}
 	
-		// Update is called once per frame
-		void Update ()
-		{
-				txt.text = "" + MultiTxt; 
+	// Update is called once per frame
+	void Update ()
+	{
+		txt.text = "" + MultiTxt; 
 				
-		}
-		void PopCurrText ()
-		{
-				PopText.text = "" + (MultiTxt + 1);
-		}
+	}
+	void PopCurrText ()
+	{
+		PopText.text = "" + (MultiTxt + 1);
+	}
 }
--- Assets/scripts/SpawnScript.cs
+++ Assets/scripts/SpawnScript.cs
@@ -5,49 +5,52 @@ using System.Collections;
 public class SpawnScript : MonoBehaviour
 {
 // Enemys
-		public GameObject[] enemy;
-		public float EnemyNum;//Numero de inimigos a serem lançados
-		public float EnemyCurrNum = 0; //numero atual de inimigos lançados
-		//public float EnemyCount;
-		//public float EnemyCountDead = 0;
+	public GameObject[] enemy;
+	public float EnemyNum;//Numero de inimigos a serem lançados
+	public float EnemyCurrNum = 0; //numero atual de inimigos lançados
+	//public float EnemyCount;
+	//public float EnemyCountDead = 0;
 // Velocidade de Spawn
-		public float spawnTime = 2;
-		Vector2 spawnPoint;
-		float x1;
-		float x2;
+	public float spawnTime = 2;
+	Vector2 spawnPoint;
+	float x1;
+	float x2;
+	float x3;//teste
 
-		void  Start ()
-		{  
-				// Call the 'addEnemy' function every 'spawnTime' seconds
-				InvokeRepeating ("addEnemy", spawnTime, spawnTime);
-		}
 
-	void Update(){
+	void  Start ()
+	{  
+		// Call the 'addEnemy' function every 'spawnTime' seconds
+		InvokeRepeating ("addEnemy", spawnTime, spawnTime);
+	}
+
+	void Update ()
+	{
 		//EnemyCount = EnemyCurrNum-1;
-		}
+	}
 
 // New function to spawn an enemy
-		void  addEnemy ()
-		{  
-				if (EnemyCurrNum <= (EnemyNum-1)) {
-						// Variables to store the X position of the spawn object
-						// See image below
-						x1 = transform.position.x - renderer.bounds.size.x / 2;
-						x2 = transform.position.x + renderer.bounds.size.x / 2;
+	void  addEnemy ()
+	{  
+		if (EnemyCurrNum <= (EnemyNum - 1)) {
+			// Variables to store the X position of the spawn object
+			// See image below
+			x1 = transform.position.x - renderer.bounds.size.x / 2;
+			x2 = transform.position.x + renderer.bounds.size.x / 2;
 
-						// Randomly pick a point within the spawn object
-						spawnPoint = new Vector2 (UnityEngine.Random.Range (x1, x2), transform.position.y);
+			// Randomly pick a point within the spawn object
+			spawnPoint = new Vector2 (UnityEngine.Random.Range (x1, x2), transform.position.y);
 
-						// Create an enemy at the 'spawnPoint' position
-						Instantiate (enemy[0], spawnPoint, Quaternion.identity);
-						EnemyCurrNum++;
-						//print (EnemyCurrNum-1);
+			// Create an enemy at the 'spawnPoint' position
+			Instantiate (enemy [0], spawnPoint, Quaternion.identity);
+			EnemyCurrNum++;
+			//print (EnemyCurrNum-1);
 						
-				}
 		}
+	}
 	//when EnemyDies
-		//public void PlusDeath()
-		//{
-			//EnemyCountDead = EnemyCountDead+1;	
-		//}
+	//public void PlusDeath()
+	//{
+	//EnemyCountDead = EnemyCountDead+1;	
+	//}
 }
