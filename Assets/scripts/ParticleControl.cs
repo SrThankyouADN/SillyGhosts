52fa3b70479f72851bdee1c52211fb9d93211bb2
SrThankyouADN <sr.thankyouadn@gmail.com>
2015-04-23 17:31:56 -0300

    Correção de Sortenig e Animação das Particulas.

---
+++ Assets/scripts/ParticleControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleControl : MonoBehaviour
+{
+		public GameObject particles;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		public void PlayParticle ()
+		{
+				particles.particleSystem.Play ();
+		}
+
+}
---
+++ Assets/scripts/ParticleSortLayerFix.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSortLayerFix : MonoBehaviour
+{
+
+		public string LayerName = "Player";
+		
+
+
+		// Use this for initialization
+		void Start ()
+		{
+				particleSystem.renderer.sortingLayerName = LayerName;
+				particleSystem.renderer.sortingOrder = -1;
+		}
+	
+		// Update is called once per frame
+		
+}
